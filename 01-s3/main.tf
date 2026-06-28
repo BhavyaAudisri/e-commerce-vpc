8811c886f1c030bc62eddddf5a4627f018682eb4
bhavya audisri <bhavyacse40@gmail.com>
2026-06-28 16:40:42 +0530

    provider updated

--- 01-s3/main.tf
+++ 01-s3/main.tf
@@ -1,4 +1,34 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "ecommerce" {
   bucket = "ecommerce-frontend-1488"
 
+}
+
+# Block all public access
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+# Disable Versioning
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+# Enable Server-Side Encryption (SSE-S3)
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
