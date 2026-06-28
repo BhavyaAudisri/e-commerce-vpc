f372904e40e2dfbf51249bc7f086dd3ad877fb21
bhavya audisri <bhavyacse40@gmail.com>
2026-06-28 15:47:10 +0530

    provider file updated

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -6,13 +6,6 @@ terraform {
       #version = "~> 5.84"
     }
   }
-
-  backend "s3" {
-    bucket         = "ecomm-aws-vpc"
-    key            = "ecomm-aws-vpctest" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
-    region         = "us-east-1"
-    dynamodb_table = "ecomm-aws-vpc"
-  }
 }
 
 provider "aws" {
