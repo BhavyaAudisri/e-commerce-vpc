f0af8722c0b04e27faffe9f18e6ef268cf199c23
bhavya audisri <bhavyacse40@gmail.com>
2026-07-05 15:11:59 +0530

    vpc naming convetnion

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -36,7 +36,6 @@ resource "aws_subnet" "public" {
 
   tags = merge(
     var.common_tags,
-    var.public_subnet_tags,
     {
       Name = "${local.resource_name}-public-${local.az_names[count.index]}"
     }
@@ -52,29 +51,11 @@ resource "aws_subnet" "private" {
 
   tags = merge(
     var.common_tags,
-    var.private_subnet_tags,
     {
       Name = "${local.resource_name}-private-${local.az_names[count.index]}"
     }
   )
 }
-
-# expense-dev-database-us-east-1a
-resource "aws_subnet" "database" {
-  count             = length(var.database_subnet_cidrs)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.database_subnet_cidrs[count.index]
-  availability_zone = local.az_names[count.index]
-
-  tags = merge(
-    var.common_tags,
-    var.database_subnet_tags,
-    {
-      Name = "${local.resource_name}-database-${local.az_names[count.index]}"
-    }
-  )
-}
-
 
 resource "aws_eip" "nat" {
   domain = "vpc"
@@ -102,7 +83,6 @@ resource "aws_route_table" "public" {
 
   tags = merge(
     var.common_tags,
-    var.public_route_table_tags,
     {
       Name = "${local.resource_name}-public-rt"
     }
@@ -114,7 +94,6 @@ resource "aws_route_table" "private" {
 
   tags = merge(
     var.common_tags,
-    var.private_route_table_tags,
     {
       Name = "${local.resource_name}-private-rt"
     }
@@ -125,7 +104,6 @@ resource "aws_route_table" "database" {
   vpc_id = aws_vpc.main.id
   tags = merge(
     var.common_tags,
-    var.database_route_table_tags,
     {
       Name = "${local.resource_name}-database-rt"
     }
