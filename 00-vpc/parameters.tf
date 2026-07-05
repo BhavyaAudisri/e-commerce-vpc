097970d53c9aab1c64b9abd2bf53acf935febc02
bhavya audisri <bhavyacse40@gmail.com>
2026-07-05 15:17:16 +0530

    vpc naming convetnion

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -7,24 +7,24 @@ resource "aws_ssm_parameter" "vpc_id" {
 resource "aws_ssm_parameter" "eip_id" {
   name  = "/${var.project_name}/${var.environment}/eip_id"
   type  = "String"
-  value = aws_vpc.vpc.elastic_ip
+  value = aws_eip.nat.elastic_ip
  #overwrite = true
 }
 resource "aws_ssm_parameter" "public_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/public-subnet_ids"
   type = "StringList"
-  value = join(",", aws_vpc.vpc.public_subnet_ids)
+  value = join(",", aws_subnet.public.public_subnet_ids)
   #overwrite = true
 }
 resource "aws_ssm_parameter" "private_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/private-subnet_ids"
   type = "StringList"
-  value = join(",", aws_vpc.vpc.private_subnet_ids)
+  value = join(",", aws_subnet.private.private_subnet_ids)
   #overwrite = true
 }
 resource "aws_ssm_parameter" "database_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/database-subnet_ids"
   type = "StringList"
-  value = join(",", aws_vpc.vpc.database_subnet_ids)
+  value = join(",", aws_subnet.database.database_subnet_ids)
   #overwrite = true
  }
