695b276348ef68b7b8029d04ab8f7d955391c2e7
bhavya audisri <bhavyacse40@gmail.com>
2026-07-05 15:30:51 +0530

    vpc naming convetnion

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -72,9 +72,7 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "example" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id
-  Name = "ecommerce-nat-gateway"
-
-  
+   
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.main]
