ea1422293626ebf67d4a3822425cc301e28e71e5
bhavya audisri <bhavyacse40@gmail.com>
2026-07-05 15:00:44 +0530

    vpc naming convetnion

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -1,4 +1,4 @@
-/* resource "aws_ssm_parameter" "vpc_id" {
+resource "aws_ssm_parameter" "vpc_id" {
   name  = "/${var.project_name}/${var.environment}/vpc_id"
   type  = "String"
   value = module.vpc.vpc_id
@@ -28,4 +28,3 @@ resource "aws_ssm_parameter" "database_subnet_ids" {
   value = join(",", module.vpc.database_subnet_ids)
   #overwrite = true
  }
- */
--- 00-vpc/var.tf
+++ 00-vpc/var.tf
@@ -1,9 +1,5 @@
 variable "project_name" {
-  default = "ecommerce"
-}
-
-variable "environment" {
-  default = "dev"
+  default = "ecommerce-vpc"
 }
 
 variable "vpc_cidr" {
@@ -35,6 +31,39 @@ variable "private_subnet_cidrs" {
 variable "database_subnet_cidrs" {
   default = ["10.10.21.0/24", "10.10.22.0/24"]
 }
-variable "public_subnet_tags" {
-  default = {}
+
+
+variable "enable_dns_hostnames" {
+  default = true
+}
+
+variable "igw_tags" {
+  default = "ecommerce-igw"
+}
+
+variable "public_subnet_cidrs" {
+  type = list(any)
+  validation {
+    condition     = length(var.public_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid public subnet CIDR"
+  }
+   default = ["10.10.1.0/24", "10.10.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type = list(any)
+  validation {
+    condition     = length(var.private_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid private subnet CIDR"
+  }
+  default = ["10.10.11.0/24", "10.10.12.0/24"]
+}
+
+variable "database_subnet_cidrs" {
+  type = list(any)
+  validation {
+    condition     = length(var.database_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid database subnet CIDR"
+  }
+  default = ["10.10.21.0/24", "10.10.22.0/24"]
 }
