9486b30660fa7ffd59b65e9c414945cb574b2658
bhavya audisri <bhavyacse40@gmail.com>
2026-07-09 17:23:50 +0530

    cdn tf file uploaded

--- 50-ec2/parameters.tf
+++ 50-ec2/parameters.tf
@@ -1,10 +1,10 @@
 data "aws_ssm_parameter" "access_key" {
   name = "/ecommerce/aws/access-key"
-  overwrite = true
+  #overwrite = true
 }
 
 data "aws_ssm_parameter" "secret_key" {
   name            = "/ecommerce/aws/secret-key"
   with_decryption = true
-  overwrite = true
+  #overwrite = true
 }
