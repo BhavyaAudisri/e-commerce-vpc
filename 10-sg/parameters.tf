9a197b4a68c5f57408fd2acd1463063f57c2fea4
bhavya audisri <bhavyacse40@gmail.com>
2026-07-09 16:02:38 +0530

    s3 bucket tf file uploaded

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -4,3 +4,27 @@ resource "aws_ssm_parameter" "ecommerce_sg_id" {
   value = module.ecommerce_sg.sg_id
  # overwrite = true
 }
+resource "aws_ssm_parameter" "rds_sg_id" {
+  name  = "/${var.project_name}/rds_sg_id"
+  type  = "String"
+  value = module.rds_sg_id.sg_id
+ # overwrite = true
+}
+resource "aws_ssm_parameter" "ecommerce_alb_sg" {
+  name  = "/${var.project_name}/ecommerce_alb_sg"
+  type  = "String"
+  value = module.ecommerce_alb_sg.sg_id
+ # overwrite = true
+}
+resource "aws_ssm_parameter" "ecommerce_ecs_sg" {
+  name  = "/${var.project_name}/ecommerce_ecs_sg"
+  type  = "String"
+  value = module.ecommerce_ecs_sg.sg_id
+ # overwrite = true
+}
+resource "aws_ssm_parameter" "vpclink_sg" {
+  name  = "/${var.project_name}/vpclink_sg"
+  type  = "String"
+  value = module.vpclink_sg.sg_id
+ # overwrite = true
+}
