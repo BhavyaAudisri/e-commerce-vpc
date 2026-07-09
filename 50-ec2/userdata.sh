381b33e6e42dd751aa7c1e83477416de0dd0de68
bhavya audisri <bhavyacse40@gmail.com>
2026-07-09 22:41:11 +0530

    changes updated in ec2

--- 50-ec2/userdata.sh
+++ 50-ec2/userdata.sh
@@ -16,7 +16,17 @@ apt-get install -y \
     git \
     nodejs \
     npm \
-    awscli
+    unzip \
+    
+cd /tmp
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip
+
+unzip -q awscliv2.zip
+
+./aws/install
+
+aws --version
 
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
