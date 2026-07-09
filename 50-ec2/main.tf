e5393549ace23dbdd83b64aa25e0c280f866190e
bhavya audisri <bhavyacse40@gmail.com>
2026-07-09 22:24:23 +0530

    changes updated in ec2

--- 50-ec2/main.tf
+++ 50-ec2/main.tf
@@ -5,6 +5,24 @@ resource "aws_instance" "ecommerce" {
   vpc_security_group_ids = [data.aws_ssm_parameter.ecommerce_sg_id.value]
   key_name               = "e-commerce"
   associate_public_ip_address = true
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"                     # Amazon Linux: ec2-user
+    host        = self.public_ip
+    private_key = file("${path.module}/ecommerce.pem")
+  }
+   provisioner "file" {
+    source      = "${path.module}/userdata.sh"
+    destination = "/tmp/userdata.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/userdata.sh",
+      "sudo /tmp/userdata.sh"
+    ]
+  }
+
   user_data = templatefile("${path.module}/userdata.sh", {
   access_key   = data.aws_ssm_parameter.access_key.value
   secret_key   = data.aws_ssm_parameter.secret_key.value
